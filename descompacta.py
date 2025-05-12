b82a32edfe642c8d6dd694eadd69829202e756e1
arthurrfrancis <111395840+arthurrfrancis@users.noreply.github.com>
2025-05-12 19:06:57 -0300

    Update descompacta.py

--- descompacta.py
+++ descompacta.py
@@ -2,28 +2,25 @@ def descompactar_rle(texto):
     resultado = ""
     numero = ""
 
-    # Descompressão
     for char in texto:
         if char.isdigit():
-            numero += char  # Acumula números
+            numero += char 
         else:
-            resultado += char * int(numero)  # Repete o caractere
-            numero = ""  # Reseta o contador
+            resultado += char * int(numero)
+            numero = "" 
     
     return resultado
 
-# Função para descompactar o arquivo .txt (se necessário)
 def descompactar_arquivo_txt_rle(entrada, saida):
     with open(entrada, 'r') as f:
-        texto = f.read()  # Lê o conteúdo compactado
-    descompactado = descompactar_rle(texto)  # Descompacta o conteúdo
+        texto = f.read() 
+    descompactado = descompactar_rle(texto)
     with open(saida, 'w') as f:
-        f.write(descompactado)  # Escreve o conteúdo descompactado no novo arquivo
+        f.write(descompactado)
 
-entrada = 'arquivo1.txt'  # Arquivo de entrada
-saida = 'compactado.txt'  # Arquivo de saída (compactado)
+entrada = 'arquivo1.txt'  
+saida = 'compactado.txt'  
 
-# Teste de descompactação (se necessário)
 saida_descompactado = 'descompactado.txt'
 descompactar_arquivo_txt_rle(saida, saida_descompactado)
 print(f"Arquivo {saida} foi descompactado para {saida_descompactado}.")
